5c582a50bbe8ce208b4f3980ce5dfb12e43fc313
harun12 <harun.korkmaz@intimeinfo.com.tr>
2023-03-01 10:34:48 +0300

    rm seccond web ui project

--- murat_yuc_asp_1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace murat_yuc_asp_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
