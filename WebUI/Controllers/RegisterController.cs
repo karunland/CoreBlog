09b059e86bcc57ad6eefa4bdc41d9c5af64a6f02
harun <1harunkorkmaz@gmail.com>
2023-03-10 10:28:25 +0300

    init

--- WebUI/Controllers/RegisterController.cs
+++ WebUI/Controllers/RegisterController.cs
@@ -1,13 +1,17 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace error_fix_package.Controllers
 {
     public class RegisterController : Controller
     {
+        WriterManager _writerManager = new WriterManager(new EfWriterRepository());
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -17,8 +21,23 @@ namespace error_fix_package.Controllers
         [HttpPost]
         public IActionResult Index(Writer writer)
         {
-
-            return View();
+            WriterValidator vw = new WriterValidator();
+            ValidationResult results = vw.Validate(writer);
+            if (results.IsValid)
+            {
+                writer.WriterStatus = true;
+                writer.WriterAbout = "test_writerabout";
+                _writerManager.WriterAdd(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
         }
     }
 }
