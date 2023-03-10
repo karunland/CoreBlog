f55b9633cb8a7154a8e423b8309495dff9e5c396
harun <1harunkorkmaz@gmail.com>
2023-03-10 10:30:17 +0300

    webui: add writer's blogs to blog detail page

---
+++ WebUI/ViewComponents/Blog/WriterLastBlogs.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace WebUI.ViewComponents.Blog
+{
+    public class WriterLastBlogs : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        
+        public IViewComponentResult Invoke()
+        {
+            var vals = bm.GetBlogByWriter(4);
+            return View(vals);
+        }
+    }
+}
