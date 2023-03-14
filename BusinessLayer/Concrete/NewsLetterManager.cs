85ead93defe6d90d19f071c60ac92ac88d42775f
harun <1harunkorkmaz@gmail.com>
2023-03-14 16:52:35 +0300

    feat: add newsletter repo, interface, service, manager

---
+++ BusinessLayer/Concrete/NewsLetterManager.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class NewsLetterManager : INewsLetterService
+    {
+        INewsLetterDal _newsLetter;
+        public NewsLetterManager(INewsLetterDal newsLetter)
+        {
+            _newsLetter = newsLetter;
+        }
+        public void AddNewsLetter(NewsLetter model)
+        {
+            _newsLetter.Insert(model);
+        }
+    }
+}
