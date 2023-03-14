f380e081394cd524183744154879b4604ee24a7c
harun <1harunkorkmaz@gmail.com>
2023-03-14 17:12:22 +0300

    gorev: fluent validation password new rules

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -18,7 +18,9 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Password cannot be empty");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Please enter at least two characters");
             RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Please enter maximum of 50 characters");
-
+            RuleFor(x => x.WriterPassword).Matches(@"[A-Z]+").WithMessage("Password should contain at least one uppercase letter");
+            RuleFor(x => x.WriterPassword).Matches(@"[a-z]+").WithMessage("Password should contain at least one lowercase letter");
+            RuleFor(x => x.WriterPassword).Matches(@"[0-9]+").WithMessage("Password should contain at least one digit");
         }
     }
 }
