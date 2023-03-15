070fbcc413ce35b3c2d7afc3f486edee08abd3e3
harun <1harunkorkmaz@gmail.com>
2023-03-15 15:57:44 +0300

    feat: add contact service and pages

---
+++ WebUI/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact person)
+        {
+            person.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            person.ContactStatus = true;
+            cm.ContactAdd(person);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
