698bd6ea9ec1be1f8b87a33033330cd8abf79899
harun <1harunkorkmaz@gmail.com>
2023-03-15 17:26:37 +0300

    add authorization Video 44

---
+++ WebUI/Controllers/WriterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    [Authorize]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile(int? id)
+        {
+            return View();
+        }
+    }
+}
