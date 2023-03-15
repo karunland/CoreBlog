3c1c97e0ada2ff19ea2dc5fc4b6c91bba3a7ed68
harun <1harunkorkmaz@gmail.com>
2023-03-15 15:56:48 +0300

    feat: add about pages and sevice

---
+++ WebUI/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager am = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var items = am.GetList();
+            return View(items);
+        }
+
+        [HttpGet]
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}
