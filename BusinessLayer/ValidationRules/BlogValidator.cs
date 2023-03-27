19381fa30162699bca2241a58f713504349844cb
harun <1harunkorkmaz@gmail.com>
2023-03-27 14:54:58 +0300

    56: error: add blog on writer page

---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Cannot be empty");
+            RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Cannot be empty");
+            RuleFor(x => x.BlogThumbnailImage).NotEmpty().WithMessage("Cannot be empty");
+            RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("Cannot be longer than 150 characters");
+            RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("Cannot be shorther than 5 characters");
+             
+        }
+    }
+}
--- WebUI/Controllers/BlogController.cs
+++ WebUI/Controllers/BlogController.cs
@@ -1,6 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebUI.Controllers
 {
@@ -12,7 +16,7 @@ namespace WebUI.Controllers
         {
             return View(_blogManager.GetList());
         }
-        
+
         public IActionResult Details(int id)
         {
             return View(_blogManager.GetBlogById(id));
@@ -21,6 +25,43 @@ namespace WebUI.Controllers
         public IActionResult GetBLogByWriter(int id)
         {
             return View(_blogManager.GetBlogByWriter(4));
+        }
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            CategoryManager cm = new CategoryManager(new EfCategoryReposiyory());
+            List<SelectListItem> categoryValues = (from x in cm.GetList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CatergoryId.ToString()
+                                                   }).ToList();
+            ViewBag.id = 4;
+            ViewBag.cv = categoryValues;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p, int id)
+        {
+            BlogValidator bv = new BlogValidator();
+            var results = bv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterId = 4;
+                _blogManager.TAdd(p);
+                return RedirectToAction("GetBlogByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
         }
     }
 }
