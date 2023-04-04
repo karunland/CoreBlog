60d32a7d7f20c4dc6b8d08506f63f4ee377c196a
harun <1harunkorkmaz@gmail.com>
2023-04-04 16:57:38 +0300

    63: dashboard add component about blog

---
+++ WebUI/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var vals = bm.GetLast3Blogs();
+            return View(vals);
+        }
+    }
+}
--- WebUI/ViewComponents/Category/CategoryListDashboard.cs
+++ WebUI/ViewComponents/Category/CategoryListDashboard.cs
@@ -1,11 +1,10 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace WebUI.ViewComponents.Category
 {
-    public class CategoryList: ViewComponent
+    public class CategoryListDashboard : ViewComponent
     {
         CategoryManager _categoryManager = new CategoryManager(new EfCategoryReposiyory());
         public IViewComponentResult Invoke()
