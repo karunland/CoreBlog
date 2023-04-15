fe14001d257842a5c8bd33dae4c76cc4f8030f22
harun <1harunkorkmaz@gmail.com>
2023-04-15 03:19:52 +0300

    79: list the notifications

--- WebUI/Controllers/NotificationController.cs
+++ WebUI/Controllers/NotificationController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager nm = new NotificationManager(new EfNotificationRepository());
+
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+
+            return View(nm.GetList());
         }
     }
 }
