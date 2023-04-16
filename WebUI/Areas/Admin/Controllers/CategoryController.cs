3f34abb0e3e11ba35416ae4a7b795f75c3022026
harun <1harunkorkmaz@gmail.com>
2023-04-17 01:40:39 +0300

    91: admin area: list categories

--- WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager _categoryManager = new CategoryManager(new EfCategoryReposiyory());
         [Area("Admin")]
         public IActionResult Index()
         {
-            return View();
+            var items = _categoryManager.GetList();
+            return View(items);
         }
     }
 }
