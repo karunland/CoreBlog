8272b6e9c37e5069df3a4b3106403f0dbbd88e32
harun <1harunkorkmaz@gmail.com>
2023-04-17 02:00:53 +0300

    92: add pagination to admin/category/index page

--- WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,21 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
     public class CategoryController : Controller
     {
         CategoryManager _categoryManager = new CategoryManager(new EfCategoryReposiyory());
-        [Area("Admin")]
-        public IActionResult Index()
+
+        [HttpGet("Index/{page?}")]
+        public IActionResult Index(int page = 1)
         {
-            var items = _categoryManager.GetList();
+            var items = _categoryManager.GetList().ToPagedList(page, 3);
             return View(items);
         }
     }
