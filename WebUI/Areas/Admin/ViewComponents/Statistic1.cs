09833d875819f454f09827f6e6e9242297ab3738
harun <1harunkorkmaz@gmail.com>
2023-04-24 23:46:24 +0300

    99: Statistic 1

---
+++ WebUI/Areas/Admin/ViewComponents/Statistic1.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Areas.Admin.ViewComponents
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context context = new Context();
+        
+        public  IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount = bm.GetList().Count();
+            ViewBag.MessageCount = context.Contacts.Count();
+            ViewBag.CommentCount = context.Comments.Count();
+            return View();
+        }
+    }
+}
