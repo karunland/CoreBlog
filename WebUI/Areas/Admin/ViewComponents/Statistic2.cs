1d1b8217fab7a09f59bcdde79ce66046f32d3328
harun <1harunkorkmaz@gmail.com>
2023-04-25 01:14:31 +0300

    100: statistic 2

---
+++ WebUI/Areas/Admin/ViewComponents/Statistic2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Areas.Admin.ViewComponents
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Blogs.OrderByDescending(x => x.Id)?
+                                .Take(1)
+                                .Select(y => y.BlogTitle)
+                                .FirstOrDefault();
+            return View();
+        }
+    }
+}
