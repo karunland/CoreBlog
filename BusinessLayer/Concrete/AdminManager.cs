3d3f7fb3d1944ab4b6c1da2e127409ae11e68f29
harun <1harunkorkmaz@gmail.com>
2023-04-25 02:32:41 +0300

    102: Statistic 3, even tho class doesnt cover that ?

---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        readonly IAdminDal _adminDal;
+        
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+
+        public Admin GetByIdd(int id)
+        {
+            return _adminDal.GetById(id);
+        }
+
+        public List<Admin> GetList()
+        {
+            return _adminDal.GetListAll();
+        }
+
+        public void TAdd(Admin t)
+        {
+            _adminDal.Insert(t);
+        }
+
+        public void TDelete(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebUI/Areas/Admin/ViewComponents/Statistic4.cs
+++ WebUI/Areas/Admin/ViewComponents/Statistic4.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using DocumentFormat.OpenXml.Vml.Spreadsheet;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Areas.Admin.ViewComponents
 {
     public class Statistic4 : ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Admins.Where(x=>x.Id ==2).FirstOrDefault();
             return View();
         }
     }
