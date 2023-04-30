a73508eaf11740dc8bfc70b7316ae6d4689344ee
harun <1harunkorkmaz@gmail.com>
2023-05-01 00:41:06 +0300

    103: api weather admin page

--- WebUI/Areas/Admin/ViewComponents/Statistic1.cs
+++ WebUI/Areas/Admin/ViewComponents/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace WebUI.Areas.Admin.ViewComponents
 {
@@ -12,9 +13,14 @@ namespace WebUI.Areas.Admin.ViewComponents
         
         public  IViewComponentResult Invoke()
         {
+            string apiKey = "0fd75240baf47c86acec3fdeb7e68895";
+            string city = "istanbul";
+            string apiPath = $"https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=0fd75240baf47c86acec3fdeb7e68895&mode=xml ";
             ViewBag.BlogCount = bm.GetList().Count();
             ViewBag.MessageCount = context.Contacts.Count();
             ViewBag.CommentCount = context.Comments.Count();
+            XDocument x = XDocument.Load(apiPath);
+            ViewBag.weather = Convert.ToDouble(x.Descendants("temperature").ElementAt(0).Attribute("value").Value) - 273;
             return View();
         }
     }
