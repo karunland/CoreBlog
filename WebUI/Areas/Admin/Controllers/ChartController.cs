8b31243a902103a7a76eccc589a8020f6e33b9d0
harun <1harunkorkmaz@gmail.com>
2023-05-01 13:35:46 +0300

    104: google chart 2

---
+++ WebUI/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUI.Areas.Admin.Models;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CategoryChart()
+        {
+            var mlist = new List<CategoryClass>();
+
+            mlist.Add(new CategoryClass
+            {
+                categoryname = "Tech",
+                categorycount = 4
+            }
+            );
+            mlist.Add(new CategoryClass
+            {
+                categoryname = "Software",
+                categorycount = 1
+            }
+            );
+            mlist.Add(new CategoryClass
+            {
+                categoryname = "Linux",
+                categorycount = 2
+            }
+            );
+            return Json(new { jsonlist = mlist } );
+        }
+    }
+}
---
+++ WebUI/Areas/Admin/Models/CategoryClass.cs
@@ -0,0 +1,8 @@
+﻿namespace WebUI.Areas.Admin.Models
+{
+    public class CategoryClass
+    {
+        public string categoryname { get; set; }
+        public int categorycount { get; set; }
+    }
+}
