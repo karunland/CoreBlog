15437863acddf70918e43e1e55f902723a717dc3
harun <1harunkorkmaz@gmail.com>
2023-05-11 15:03:40 +0300

    115: first api

---
+++ BlogApi/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using BlogApi.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            using (var context = new Context())
+            {
+                var items = context.Employees.ToList();
+                return Ok(items);
+            }
+        }
+    }
+}
