f749e57d2206ee96f542132f86566fdcfe559275
harun <1harunkorkmaz@gmail.com>
2023-06-01 11:18:11 +0300

    125: Login with Identity

--- WebUI/Controllers/LoginController.cs
+++ WebUI/Controllers/LoginController.cs
@@ -2,39 +2,63 @@
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index(Writer p)
+        public async Task<IActionResult> Index(UserSignInViewModel model)
         {
-            Context c = new Context();
-            var dataValue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
-            if (dataValue != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim> {
-                    new Claim (ClaimTypes.Name, p.WriterMail)
-                };
-                
-                var userIdentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                await HttpContext.SignInAsync(principal);
-                
-                HttpContext.Session.SetString("username", p.WriterMail);
-                return RedirectToAction("Index", "Blog");
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                return RedirectToAction("Index", "Login");
             }
-            return View();
+            return View("Index");
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(Writer p)
+        //{
+        //    Context c = new Context();
+        //    var dataValue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+        //    if (dataValue != null)
+        //    {
+        //        var claims = new List<Claim> {
+        //            new Claim (ClaimTypes.Name, p.WriterMail)
+        //        };
+                
+        //        var userIdentity = new ClaimsIdentity(claims, "a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+        //        await HttpContext.SignInAsync(principal);
+                
+        //        HttpContext.Session.SetString("username", p.WriterMail);
+        //        return RedirectToAction("Index", "Blog");
+        //    }
+        //    return View();
+        //}
     }
 }
---
+++ WebUI/Models/UserSignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Please enter Name and Surname!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Password!")]
+        public string Password { get; set; }
+    }
+}
