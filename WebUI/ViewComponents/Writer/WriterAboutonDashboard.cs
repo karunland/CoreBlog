532282eeb4b428748a333a491b7b93dac64fc5ff
harun <1harunkorkmaz@gmail.com>
2023-06-01 11:52:27 +0300

    126: get user from IdentityUser by UserName

--- WebUI/ViewComponents/Writer/WriterAboutonDashboard.cs
+++ WebUI/ViewComponents/Writer/WriterAboutonDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.ViewComponents.Writer
@@ -8,13 +10,24 @@ namespace WebUI.ViewComponents.Writer
     public class WriterAboutonDashboard : ViewComponent
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
+        private readonly UserManager<AppUser> _userManager;
+
         Context c = new Context();
+
+        public WriterAboutonDashboard(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IViewComponentResult Invoke(int id)
         {
-            var usermail = User.Identity?.Name;
-            if (usermail == null)
+            var name = User.Identity?.Name;
+            if (name == null)
                 return View(wm.GetWriterbyId(1));
-            var userid = c.Writers.Where(x=> x.WriterMail == usermail).Select(y=>y.Id).FirstOrDefault();
+            
+            var userid = c.Writers.Where(x=> x.WriterName == name).Select(y=>y.Id).FirstOrDefault();
+            var mail = c.Writers.Where(x=> x.WriterName == name).Select(y=>y.WriterMail).FirstOrDefault();
+            
             var val = wm.GetByIdd(userid);
             return View(val);
         }
