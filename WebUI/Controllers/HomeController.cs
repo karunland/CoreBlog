0a879090c808c6299c548d1ddf42c43d59a25d57
harun <1harunkorkmaz@gmail.com>
2023-06-14 17:30:21 +0300

    home page design

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+
+        public IActionResult Index()
+        {
+            return View(_blogManager.GetList());
+        }
+    }
+}
